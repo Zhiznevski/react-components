6891b1c7a8f9c0f824969462c9162b90b25429b9
Zhiznevski <w.heisenberggg@gmail.com>
2023-10-26 14:24:49 +0300

    feat: add search to project

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,80 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
+import { createRef } from 'react';
+import { Component } from 'react';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+interface Person<T> {
+  name: T;
+  height: T;
+  mass: T;
+  hair_color: T;
+  skin_color: T;
+  eye_color: T;
+  birth_year: T;
+  gender: T;
+  homeworld: T[];
+  films: T[];
+  species: T[];
+  vehicles: T[];
+  starships: T[];
+  created: T;
+  edited: T;
+  url: T;
 }
 
-export default App;
+export default class App extends Component {
+  state: { persons: Person<string>[]; value: string } = {
+    persons: [],
+    value: '',
+  };
+  inputRef = createRef<HTMLInputElement>();
+
+  submitHandler = (event: React.SyntheticEvent) => {
+    event.preventDefault();
+    const target = this.inputRef.current;
+    if (target) {
+      this.setState((prevState) => ({ ...prevState, value: target.value }));
+    }
+  };
+
+  componentDidMount(): void {
+    fetch(`https://swapi.dev/api/people/`).then((responce) =>
+      responce.json().then((data) => {
+        console.log(data);
+        this.setState((prevState) => ({ ...prevState, persons: data.results }));
+      })
+    );
+  }
+
+  componentDidUpdate(
+    prevProps: Readonly<object>,
+    prevState: { persons: Person<string>[]; value: string }
+  ): void {
+    if (prevState.value !== this.state.value) {
+      fetch(`https://swapi.dev/api/people/?search=${this.state.value}`).then(
+        (responce) =>
+          responce.json().then((data) => {
+            console.log(data);
+            this.setState((prevState) => ({
+              ...prevState,
+              persons: data.results,
+            }));
+          })
+      );
+    }
+  }
+
+  render() {
+    return (
+      <div style={{ marginLeft: '45vw' }}>
+        <form onSubmit={this.submitHandler}>
+          <input ref={this.inputRef} type="search" placeholder="search"></input>
+          <button type="submit">search</button>
+        </form>
+        <div>
+          {this.state.persons?.map((person) => (
+            <div key={person.name}>{person.name}</div>
+          ))}
+        </div>
+      </div>
+    );
+  }
+}

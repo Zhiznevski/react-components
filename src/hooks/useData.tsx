67e7d6f64996d0cf5625f2259202cc279d032ddf
Zhiznevski <w.heisenberggg@gmail.com>
2023-11-20 01:50:46 +0300

    fix: fix types errors

--- src/hooks/useData.tsx
+++
@@ -1,7 +0,0 @@
-import { useContext } from 'react';
-import { AppContext, AppContextType } from '../Context/AppContext';
-
-export const useData = (): AppContextType | null => {
-  const context = useContext(AppContext);
-  return context;
-};
